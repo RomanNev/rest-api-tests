9835870602c35d1dd27215d01397f926f870d548
RomanGolub <romannovoselovcl@gmail.com>
2022-04-17 21:41:14 +0300

    added ExampleDemowebshopTests

--- src/test/java/tests/ExampleApiTest.java
+++ src/test/java/tests/ExampleApiTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class exampleApiTest {
+public class ExampleApiTest {
     @Test
     void checkTotal() {
         given()
