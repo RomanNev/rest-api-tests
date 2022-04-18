43308a62ddb5620c4cca497c339bb4dd6140a947
RomanGolub <romannovoselovcl@gmail.com>
2022-04-19 00:28:44 +0300

    added test for adding to wish list by unauthorized user

--- src/test/java/tests/DemoshopApiTest.java
+++ src/test/java/tests/DemoshopApiTest.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 
 public class DemoshopApiTest {
@@ -24,7 +25,7 @@ public class DemoshopApiTest {
     }
 
     @Test
-    void LoginOldUserTest() {
+    void loginOldUserTest() {
         String authorizationCookie =
                 given()
                         .contentType("application/x-www-form-urlencoded; charset=UTF-8")
@@ -46,7 +47,7 @@ public class DemoshopApiTest {
     }
 
     @Test
-    void LoginOutUserTest() {
+    void loginOutUserTest() {
         String authorizationCookie =
                 given()
                         .contentType("application/x-www-form-urlencoded; charset=UTF-8")
@@ -66,7 +67,26 @@ public class DemoshopApiTest {
         $(".ico-logout").click();
         $(".header-links-wrapper").shouldNotHave(text("golubtestuser@test.com"));
 
+    }
+
+    @Test
+    void addItemWishListCardUnauthorizedUser() {
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .body("giftcard_2.RecipientName=&" +
+                        "giftcard_2.RecipientEmail=&" +
+                        "giftcard_2.SenderName=&" +
+                        "giftcard_2.SenderEmail=&" +
+                        "giftcard_2.Message=&" +
+                        "addtocart_2.EnteredQuantity=1")
+                .when()
+                .post("http://demowebshop.tricentis.com/addproducttocart/details/2/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", is(false));
 
     }
+
 
 }
