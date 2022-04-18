2a06af9abc3baaf1e269017b18026594e8923f73
RomanGolub <romannovoselovcl@gmail.com>
2022-04-18 23:43:35 +0300

    added check login user and test login out user

--- src/test/java/tests/DemoshopApiTest.java
+++ src/test/java/tests/DemoshopApiTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
@@ -27,7 +29,7 @@ public class DemoshopApiTest {
                 given()
                         .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                         .formParam("Email", "golubtestuser@test.com")
-                        .formParam("Password","golubtestuser1" )
+                        .formParam("Password", "golubtestuser1")
                         .when()
                         .post(URL + "login")
                         .then()
@@ -35,9 +37,36 @@ public class DemoshopApiTest {
                         .extract()
                         .cookie("NOPCOMMERCE.AUTH");
 
-        open("/wishlist");
+        open("/wishlist"); // открыть страницу для прокидывания куки
         getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", authorizationCookie));
-        open("");
+        open("");// открыть главную страницу для поиска залогиненого пользователя
+        $(".account").shouldHave(text("golubtestuser@test.com"));
+
 
     }
+
+    @Test
+    void LoginOutUserTest() {
+        String authorizationCookie =
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .formParam("Email", "golubtestuser@test.com")
+                        .formParam("Password", "golubtestuser1")
+                        .when()
+                        .post(URL + "login")
+                        .then()
+                        .statusCode(302)
+                        .extract()
+                        .cookie("NOPCOMMERCE.AUTH");
+
+        open(""); // открыть страницу для прокидывания куки
+        getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", authorizationCookie));
+        open("");// открыть главную страницу
+
+        $(".ico-logout").click();
+        $(".header-links-wrapper").shouldNotHave(text("golubtestuser@test.com"));
+
+
+    }
+
 }
